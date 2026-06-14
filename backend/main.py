beb767d9a628f92ea036f2898a0b4da7a1f26bf8
ethaan19 <ethanmaciast19@gmail.com>
2026-06-14 23:47:34 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -36,7 +36,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://localhost:8000",
-        "https://football-predictor.vercel.app",  # Tu URL de Vercel
+        "https://football-predictor-hazel.vercel.app/",
         "https://football-predictor-production-9561.up.railway.app"
     ],
     allow_credentials=True,
